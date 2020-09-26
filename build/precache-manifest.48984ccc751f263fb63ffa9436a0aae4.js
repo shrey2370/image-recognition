3e14b187296451e575e0e63cd2b482df68651dcf
shrey233 <64401792+shrey233@users.noreply.github.com>
2020-09-26 12:59:59 +1000

    finallly done

---
+++ build/precache-manifest.48984ccc751f263fb63ffa9436a0aae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e3c88533ea4959d59ec0ae42787f32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "212ebda1f5e7ecfa5284",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "80834abe9a7cb61400b4",
+    "url": "/static/css/main.2e8d1a2a.chunk.css"
+  },
+  {
+    "revision": "212ebda1f5e7ecfa5284",
+    "url": "/static/js/2.3935c922.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.3935c922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80834abe9a7cb61400b4",
+    "url": "/static/js/main.b7bf8300.chunk.js"
+  },
+  {
+    "revision": "bace1e5c250713232b62",
+    "url": "/static/js/runtime-main.88daa14b.js"
+  }
+]);
